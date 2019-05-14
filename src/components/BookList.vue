d4ab802ac3333314ede44ad3f6ab8a26d8137b58
leo de blaauw <leo.de.blaauw@ordina.nl>
2019-05-14 07:47:21 +0200

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,13 +1,23 @@
-<template>
-
-</template>
+  <template>
+    <div>
+      <h1>{{title }}</h1>
+    </div>
+  </template>
 
 <script>
 export default {
-
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books"
+    }
+  }
 }
 </script>
 
 <style>
-
+h1,
+h2 {
+  font-weight: normal;
+}
 </style>
